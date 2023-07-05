690e2d616079ee8fc758da5224d9677126a57f1a
Мария Лапикова <mashik_p@mail.ru>
2023-07-05 17:57:48 +0300

    added model

--- yacut/models.py
+++ yacut/models.py
@@ -4,4 +4,7 @@ from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Colomn(db.Integer, primary_key=True)
+    original = db.Colomn(db.String(256), nullable=False)
+    short = db.Colomn(db.String(6), nullable=False)
+    timestamp = db.Colomn(db.DateTime, index=True, default=datetime.utcnow)

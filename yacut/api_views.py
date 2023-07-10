c9a843874512e089f165583317996c3b8d229be0
Мария Лапикова <mashik_p@mail.ru>
2023-07-11 02:03:18 +0300

    added api funcs

---
+++ yacut/api_views.py
@@ -0,0 +1,57 @@
+from http import HTTPStatus
+
+from flask import jsonify, request, url_for
+
+from . import app
+from .error_handlers import InvalidAPIUsage
+from .models import URLMap
+from .utils import get_unique_short_id
+
+
+@app.route('/api/id/<short_id>/', methods=('GET',))
+def get_url(short_id):
+    url_map_obj = URLMap.query.filter_by(short=short_id).first()
+    if url_map_obj is None:
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
+    response = {'url': url_map_obj.original}
+    return jsonify(response), HTTPStatus.OK
+
+
+@app.route('/api/id/', methods=('POST',))
+def create_id():
+    data = request.get_json()
+
+    if not data:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+    if 'url' not in data:
+        raise InvalidAPIUsage('\"url\" является обязательным полем!')
+
+    original = data['url']
+
+    if data.get('custom_id') is None or data.get('custom_id') == '':
+        data['custom_id'] = get_unique_short_id()
+
+    short = data['custom_id']
+
+    if URLMap.query.filter_by(short=short).first():
+        raise InvalidAPIUsage(f'Имя \"{short}\" уже занято.')
+
+    try:
+        url_map_obj = URLMap(original=original, short=short)
+        url_map_obj.save()
+    except ValueError:
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+
+    response = {
+        'url': url_map_obj.original,
+        'short_link': url_for(
+            'forwarding_view',
+            short=url_map_obj.short,
+            _external=True
+        )
+    }
+    return jsonify(response), HTTPStatus.CREATED
+
+
+
+
